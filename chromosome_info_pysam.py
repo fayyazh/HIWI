045f37e6993431f0c21dc822fb39d5e34f9839a7
fejjaz <fayyaz.hussain@gmail.com>
2017-07-06 10:35:07 +0200

    sequence end option provisioning

--- chromosome_info_pysam.py
+++ chromosome_info_pysam.py
@@ -21,21 +21,33 @@ def file_check():
     if flag == False:
         exit()
 
-def bam_reader(bam_file):
+def bam_reader(bam_file, end):
     bam_filter = []
     bam = pysam.AlignmentFile(bam_file, "rb")
     data = bam.fetch(multiple_iterators=True, until_eof=True)
-
-    for line in data:
-        if (line.is_unmapped == False and line.has_tag("XS") == False):
-            if line.is_reverse:
-                strand = '-'
-            else:
-                strand = '+'
-            bam_filter.append((line.reference_start,
-                           line.reference_start + line.reference_length,
-                           line.query_name, strand,
-                           bam.get_reference_name(line.reference_id), fastq_data[line.query_name]))
+    if end == True:
+        for line in data:
+            if (line.is_unmapped == False and line.has_tag("XS") == False):
+                if line.is_reverse:
+                    strand = '-'
+                else:
+                    strand = '+'
+                if 'N' not in fastq_data[line.query_name]:
+                    bam_filter.append((line.reference_start,
+                                       line.reference_start + line.reference_length,
+                                       line.query_name, strand,
+                                       bam.get_reference_name(line.reference_id), fastq_data[line.query_name]))
+    else:
+        for line in data:
+            if (line.is_unmapped == False and line.has_tag("XS") == False):
+                if line.is_reverse:
+                    strand = '-'
+                else:
+                    strand = '+'
+                if 'N' not in fastq_data[line.query_name]:
+                    bam_filter.append((line.reference_start,
+                                       line.query_name, strand,
+                                       bam.get_reference_name(line.reference_id), fastq_data[line.query_name]))
     bam.close()
     return bam_filter
 
@@ -46,33 +58,54 @@ def fastq_reader(fastq_file):
         update_fastq({fastq_data.id: fastq_data.seq})
     return fastq_dt
 
-def chromosome_counter(i):
+def chromosome_counter(i, end):
     # merge_checks contains reference chromosome, ref. start, ref. end and strand.
-    merge_checks = (bam_data[i][4], bam_data[i][0], bam_data[i][1], bam_data[i][5], bam_data[i][3])
-    score.update([merge_checks])
-    append_merge = merge_data.append
-    if score[merge_checks] == 1:
-        append_merge((bam_data[i][4], bam_data[i][0], bam_data[i][1], bam_data[i][2], bam_data[i][3]))
+    if end == True:
+        merge_checks = (bam_data[i][4], bam_data[i][0], bam_data[i][1], bam_data[i][5], bam_data[i][3])
+        score.update([merge_checks])
+        append_merge = merge_data.append
+        if score[merge_checks] == 1:
+            append_merge((bam_data[i][4], bam_data[i][0], bam_data[i][1], bam_data[i][2], bam_data[i][3]))
+    else:
+        merge_checks = (bam_data[i][3], bam_data[i][0], bam_data[i][4], bam_data[i][2])
+        score.update([merge_checks])
+        append_merge = merge_data.append
+        if score[merge_checks] == 1:
+            append_merge((bam_data[i][3], bam_data[i][0], bam_data[i][1], bam_data[i][2]))
     return merge_data
 
-def chromosome_info():
+def chromosome_info(end):
     chr_info = []
-    for i in range(len(bam_data)):
-        rec_id = bam_data[i][2]
-        if rec_id in fastq_data:
-            chr_info = chromosome_counter(i)
-        else:
-            print(rec_id + " ID not found in fastq file")
+    if end == True:
+        for i in range(len(bam_data)):
+            rec_id = bam_data[i][2]
+            if rec_id in fastq_data:
+                chr_info = chromosome_counter(i, end)
+            else:
+                print(rec_id + " ID not found in fastq file")
+    else:
+        for i in range(len(bam_data)):
+            rec_id = bam_data[i][1]
+            if rec_id in fastq_data:
+                chr_info = chromosome_counter(i, end)
+            else:
+                print(rec_id + " ID not found in fastq file")
     return chr_info
 
-def printing(endResult):
+def printing(endResult, end):
     with open(args.output_file, "w") as f:
-        for entry in endResult:
-            wr = ("%s\t%s\t%s\t%s\t%s\t%s" % (entry[0], entry[1], entry[2], entry[3],
-                                              score[(entry[0], entry[1], entry[2], fastq_data[entry[3]],
-                                                     entry[4])], entry[4]))
-            f.write(wr + '\n')
-
+        if end == True:
+            for entry in endResult:
+                wr = ("%s\t%s\t%s\t%s\t%s\t%s" % (entry[0], entry[1], entry[2], entry[3],
+                                                  score[(entry[0], entry[1], entry[2], fastq_data[entry[3]],
+                                                         entry[4])], entry[4]))
+                f.write(wr + '\n')
+        else:
+            for entry in endResult:
+                wr = ("%s\t%s\t%s\t%s\t%s" % (entry[0], entry[1], entry[2],
+                                                  score[(entry[0], entry[1], fastq_data[entry[2]],
+                                                         entry[3])], entry[3]))
+                f.write(wr + '\n')
 tool_description = """
 Merge PCR duplicates according to random barcode library.
 Barcodes containing uncalled base 'N' are removed.
@@ -98,11 +131,12 @@ parser.add_argument("bam_file", help="Path to bam file containing alignments.", 
 parser.add_argument("fastq_file", help="Path to fastq barcode library.", metavar='FASTQ_File')
 parser.add_argument("-o", "--output_file", required=True, help="Write results to this file.",
                     metavar='Output_File')
+parser.add_argument("-e", "--end", default=True, help="Write results to this file."
+                    )
 args = parser.parse_args()
 file_check()
-
+end = args.end
 fastq_data = fastq_reader(args.fastq_file)
-bam_data = bam_reader(args.bam_file)
-endResult = chromosome_info()
-printing(endResult)
-
+bam_data = bam_reader(args.bam_file, end)
+endResult = chromosome_info(end)
+printing(endResult, end)
